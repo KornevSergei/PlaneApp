22d4891181d86c5df07851f80a3492fdbd17db94
4Korn <4Kornev@gmail.com>
2021-02-18 04:55:25 +0300

    Commit 3

--- app/src/main/java/com/example/planeapp/GameActivity.java
+++ app/src/main/java/com/example/planeapp/GameActivity.java
@@ -23,16 +23,16 @@ public class GameActivity extends AppCompatActivity {
 
     private ImageView planeImageView;
 
-    private ImageView imageViewMeteorite;
+    private ImageView imageViewMeteorite1;
+    private ImageView imageViewMeteorite2;
+    private ImageView imageViewMeteorite3;
+    private ImageView imageViewMeteorite4;
 
     private TextView timerTextView;
 
 
-
     private CountDownTimer countDownTimer;
     public boolean timerRunning;
-
-
 
 
     private long timeLeftInMilliseconds = 60000;
@@ -46,7 +46,10 @@ public class GameActivity extends AppCompatActivity {
 
         planeImageView = findViewById(R.id.planeImageView);
         timerTextView = findViewById(R.id.timerTextView);
-        imageViewMeteorite = findViewById(R.id.imageViewMeteorite);
+        imageViewMeteorite1 = findViewById(R.id.imageViewMeteorite1);
+        imageViewMeteorite2 = findViewById(R.id.imageViewMeteorite2);
+        imageViewMeteorite3 = findViewById(R.id.imageViewMeteorite3);
+        imageViewMeteorite4 = findViewById(R.id.imageViewMeteorite4);
 
 
         JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
@@ -56,27 +59,32 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onMove(int angle, int strength) {
 
+
+                //вправо
                 if (angle > 315 || angle < 45) {
 
                     planeImageView.animate().translationXBy(20).translationYBy(0).setDuration(0);
+                    planeImageView.animate().rotation(planeImageView.getRotation() + 3);
 
-
-
-
+                    //вверх
                 } else if (angle > 45 && angle < 135) {
 
                     planeImageView.animate().translationXBy(0).translationYBy(-20).setDuration(0);
+                    planeImageView.animate().rotation(planeImageView.getRotation() + 3);
 
 
+                    //влево
                 } else if (angle > 135 && angle < 225) {
 
                     planeImageView.animate().translationXBy(-20).translationYBy(0).setDuration(0);
+                    planeImageView.animate().rotation(planeImageView.getRotation() + 3);
 
 
-
+                    //вниз
                 } else if (angle > 225 && angle < 315) {
 
                     planeImageView.animate().translationXBy(0).translationYBy(20).setDuration(0);
+                    planeImageView.animate().rotation(planeImageView.getRotation() + 3);
 
                 }
 
@@ -94,14 +102,16 @@ public class GameActivity extends AppCompatActivity {
 
     private void meteorRain() {
 
-        imageViewMeteorite.animate().translationXBy(-1000).translationYBy(2000).setDuration(3000);
+        imageViewMeteorite1.animate().translationXBy(-1000).translationYBy(2000).setDuration(7000);
 
+        imageViewMeteorite2.animate().translationXBy(-600).translationYBy(2000).setDuration(6000);
+
+        imageViewMeteorite3.animate().translationXBy(1500).translationYBy(-800).setDuration(9000);
+
+        imageViewMeteorite4.animate().translationXBy(500).translationYBy(2000).setDuration(20000);
 
 
     }
-
-
-
 
 
     private void startTimer() {
@@ -132,14 +142,11 @@ public class GameActivity extends AppCompatActivity {
         String timeLefText = "" + minutes;
         timeLefText += ":";
 
-        if(seconds < 10) timeLefText +="0";
+        if (seconds < 10) timeLefText += "0";
         timeLefText += seconds;
 
         timerTextView.setText(timeLefText);
     }
-
-
-
 
 
 }
